40ab7a0a3986546e0271ea894beee6805a0f997f
Preld <programmer.called.azathoth@gmail.com>
2015-02-01 02:13:58 +0900

    First Commit

---
+++ Assets/Scripts/SingleCamera.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * To use SingleCamera class, Dont make display in OVRManager. 
+ * So, occur two errors.
+ */
+
+using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using Ovr;
+
+public class SingleCamera : MonoBehaviour {
+
+	private Camera mainCamera;
+
+	void Start () {
+		mainCamera = this.camera;
+		OVRManager.capiHmd.RecenterPose();
+	}
+
+	void Update () {
+		OVRPose eye = GetEyePose(OVREye.Left);
+		//OVRPose rightEye = OVRManager.display.GetEyePose(OVREye.Right);
+
+		mainCamera.transform.localRotation = eye.orientation;
+		//mainCamera.transform.localPosition = 0.5f * (leftEye.position + rightEye.position);
+	}
+
+	//Dont make display in OVRManager. So, this method is needed.
+	public OVRPose GetEyePose(OVREye eye)
+	{
+		return OVR_GetRenderPose(Time.frameCount, (int)eye).ToPose();
+	}
+	
+	private const string LibOVR = "OculusPlugin";
+	[DllImport(LibOVR, CallingConvention = CallingConvention.Cdecl)]
+	private static extern Posef OVR_GetRenderPose(int frameIndex, int eyeId);
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Unity-Oculus-SingleCamera
+
+## Requirements
+Oculus Unity 4 Integration 0.4.4-beta
+
+## Notice
+This program has a bug.
+
+To use SingleCamera class, This program dont create OVRDisplay class in OVRManager.
